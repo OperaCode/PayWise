6f191d70cb09b55dfc76205fc8b384d31cab84d0
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-02-13 09:53:23 +0100

    signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,19 @@ import React from 'react';
 import './App.css'
 import LandingPage from './Pages/landingPage';
 import Header from './components/Header';
+import Signup from './Pages/Signup';
+import { Route, Routes } from "react-router-dom";
 
-const App = ()=> {
- 
+const App = () => {
+  const RenderRoutes = () => (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/register" element={<Signup/>} />
+    </Routes>
+  )
   return (
     < >
-   
-  <LandingPage />
+     {RenderRoutes()}
     </>
   )
 }
---
+++ client/src/Pages/Signup.jsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { Moon, Sun } from "lucide-react";
+import image from "../assets/signup.png"
+import logo from "../assets/paywise-logo.png"
+
+const SignUp = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add form validation logic here
+        console.log(formData);
+    };
+
+    return (
+        <div className="flex-col justify-center h-screen bg-zinc-100">
+            {/* Theme Toggle Button */}
+            <div className="flex justify-between px-4 items-center ">
+                <div className=" w-50">
+                    <img src={logo} alt="signup" className="" />
+                </div>
+                <button onClick={toggleTheme} className=" h-10  p-2 bg-gray-200 dark:bg-gray-700 rounded-2xl hover:cursor-pointer">
+                    {theme === "light" ? <Moon className="text-gray-200" /> : <Sun className="text-yellow-400" />}
+                </button>
+            </div>
+            <div className="p-8 rounded-lg shadow-lg w-full  gap-4 flex">
+                {/* Left - Form Section */}
+                <div className="w-1/2 p- flex-1">
+                    <h2 className="text-3xl text-center mb-3 font-bold">Sign up</h2>
+                    <p className=" mb-6 text-center">
+                        Hello Chief! Let’s get you started
+                    </p>
+                    <form onSubmit={handleSubmit} className="space-y-4">
+                        <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} className="w-full p-3 rounded-lg bg-gray-200  shadow-md" required />
+                        <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} className="w-full p-3 rounded-lg bg-gray-200  shadow-md" required/>
+                        <input type="email" name="email" placeholder="Email Address" onChange={handleChange} className="w-full p-3 rounded-lg bg-gray-200  shadow-md" required/>
+                        <input type="password" name="password" placeholder="Password" onChange={handleChange} className="w-full p-3 rounded-lg bg-gray-200  shadow-md" required/>
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} className="w-full p-3 rounded-lg bg-gray-200  shadow-md" required />
+                        <button type="submit" className="w-full bg-green-500 text-white py-3 rounded-lg font-semibold hover:bg-green-600 transition hover:cursor-pointer">Let's get started</button>
+                    </form>
+                    <p className="">
+                        Already have an account? <a href="/login" className="font-bold">Log in</a>
+                    </p>
+                </div>
+
+                {/* Right - Illustration */}
+                <div className="w-1/2 justify-center  hidden md:flex items-center">
+                    <img src={image} alt="Signup  Illustration" className="w-md" />
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default SignUp;
