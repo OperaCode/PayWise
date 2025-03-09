610ece7d33ecfa22c89d64f30d0167997d355196
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-03-09 17:15:13 +0100

    modals

--- client/src/Dashboards/DashBoard.jsx
+++ client/src/Dashboards/DashBoard.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState, useContext } from "react";
 import image from "../assets/category.png";
 import { UserContext } from "../context/UserContext";
-import walletImages from "../assets/autopay.png"
+import walletImages from "../assets/autopay.png";
+import blkchain from "../assets/blockchain.webp";
+import blkchain2 from "../assets/blockchain2.webp";
+import blkchain3 from "../assets/blkchain3.jpeg";
+import blkchain4 from "../assets/blockChain1.jpg";
+import blkchain5 from "../assets/darkbg.jpg";
 import {
   SmartphoneNfc,
   HandCoins,
@@ -20,6 +25,9 @@ const DashBoard = () => {
   const [walletBalance, setWalletBalance] = useState("");
   const [fundModalOpen, setFundModalOpen] = useState(false);
   const [manageTokensModalOpen, setManageTokensModalOpen] = useState(false);
+  const [p2pModalOpen, setP2pModalOpen] = useState(false);
+  const [schedulePayModalOpen, setSchedulePayModalOpen] = useState(false);
+  const [autoPayModalOpen, setAutoPayModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -39,6 +47,7 @@ const DashBoard = () => {
   return (
     <section className="p-8">
       <div className="lg:flex gap-4">
+        {/* Wallet Balance Section */}
         <div className="flex-1 h-full font-bodyFont w-full">
           <h1 className="font-bold mb-4 text-xl py-2">Current Balance:</h1>
           <div className="p-4 flex w-full justify-between rounded-lg shadow-md items-center border-4 border-neutral-500">
@@ -61,11 +70,40 @@ const DashBoard = () => {
               </button>
             </div>
           </div>
+          {/* Quick Links */}
+          <div>
+            <h1 className="font-semibold py-4 md:text-lg">Quick Links</h1>
+            <div className="flex items-center gap-6 justify-around px-6 ">
+              <div className="items-center rounded-md space-y-2 flex flex-col hover:scale-105 hover:text-cyan-900  " onClick={() => setP2pModalOpen(true)}>
+                <HandCoins className="font-extrabold" />
+                <p className="font-bold text-sm hover:cursor-pointer " >P2P</p>
+              </div>
+              <div className="items-center rounded-md space-y-2 flex flex-col hover:cursor-pointer hover:scale-105" onClick={() => setSchedulePayModalOpen(true)}>
+                <CalendarSync className="hover:text-cyan-900 font-extrabold" />
+                <p className="font-bold hover:cursor-pointer text-sm">
+                  SCHEDULE-PAY
+                </p>
+              </div>
+              <div className="items-center flex hover:cursor-pointer flex-col rounded-md space-y-2 hover:scale-105" onClick={() => setAutoPayModalOpen(true)}>
+                <SmartphoneNfc className="hover:text-cyan-900 font-extrabold" />
+                <p className="font-bold text-sm">AUTOPAY</p>
+              </div>
+              <div className="items-center flex flex-col hover:cursor-pointer rounded-md space-y-2 hover:scale-105">
+                {/* <img src={analytics} alt="" /> */}
+                <ChartNoAxesCombined classname="hover:text-cyan-900" />
+                <p className="font-bold text-sm hover:text-cyan-900">
+                  ANALYTICS
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        {/* Line Graph */}
         <div className="flex-1 w-full lg:w-50">
           <Line />
         </div>
       </div>
+      {/* Pie Chart */}
       <div className="md:flex justify-center">
         <DashPieChart />
         <div className="flex justify-center w-2/3 m-auto">
@@ -78,7 +116,10 @@ const DashBoard = () => {
 
       {/* Fund Wallet Modal */}
       {fundModalOpen && (
-        <div className="fixed inset-0 text-black flex items-center justify-center z-50 bg-neutral-500 bg-opacity-50">
+        <div className="fixed inset-0 text-black flex items-center justify-center z-50  bg-opacity-50">
+            {/* Animated Background  */}
+          <div className="absolute inset-0 animate-moving-bg bg-cover bg-center" style={{ backgroundImage: `url(${blkchain5})` }}></div>
+          
           <div className="bg-white p-6 rounded-lg shadow-lg w-2/3 md:w-1/3">
             <h2 className="text-xl font-bold mb-4">Fund Wallet</h2>
             <p>Enter the amount you want to add to your wallet.</p>
@@ -102,39 +143,208 @@ const DashBoard = () => {
 
       {/* Manage Tokens Modal */}
       {manageTokensModalOpen && (
-        <div className="fixed inset-0 text-black flex items-center justify-center bg-neutral-500 bg-opacity-50 z-50">
-            {/* Moving Gradient Background */}
-      {/* <div className="absolute inset-0 animate-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-cyan-500"></div> */}
+        <div className="fixed inset-0 text-black flex items-center justify-center bg-opacity-50 z-50">
+          {/* Moving Gradient Background */}
+          {/* <div className="absolute inset-0 animate-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-cyan-500"></div> */}
 
-      {/* Animated Background  */}
-      <div className="absolute inset-0 animate-waves bg-cover bg-center" style={{ backgroundImage: `url(${walletImages})` }}></div>
+          {/* Moving Image Background */}
+          {/* <div
+        className="absolute inset-0 galaxy-background bg-contain "
+         style={{ backgroundImage: `url(${blkchain2})` }}
+      ></div>
+      */}
 
-          <div className="bg-white p-6 rounded-lg shadow-lg w-2/3 lg:w-2xl relative">
+          {/* Animated Background  */}
+          {/* <div className="absolute inset-0 animate-waves bg-cover bg-center" style={{ backgroundImage: `url(${blkchain2})` }}></div> */}
+
+          <div
+            className="absolute inset-0 animate-moving-bg bg-cover bg-center"
+            style={{ backgroundImage: `url(${blkchain5})` }}
+          ></div>
+          <div className="stars"></div>
+
+          <div className="bg-zinc-200 p-6 rounded-lg shadow-lg w-2/3 lg:w-2xl relative">
             <h2 className="text-xl font-bold mb-4">Manage Tokens</h2>
             <p>Here you can manage your tokens.</p>
             <div className="flex justify-between w-full">
               <button
                 className=" hover:cursor-pointer mt-4 px-4 py-2 bg-cyan-700 text-white rounded-md"
-                onClick={() => setManageTokensModalOpen(false)}
+                >
+                Check your WiseCoin
+              </button>
+            </div>
+            <button
+              className=" hover:cursor-pointer mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
+              onClick={() => setManageTokensModalOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+      {/* P2P Modal */}
+      {p2pModalOpen && (
+        <div className="fixed inset-0 text-black flex items-center justify-center bg-opacity-50 z-50">
+          {/* Moving Gradient Background */}
+          {/* <div className="absolute inset-0 animate-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-cyan-500"></div> */}
+
+          {/* Moving Image Background */}
+          {/* <div
+        className="absolute inset-0 galaxy-background bg-contain "
+         style={{ backgroundImage: `url(${blkchain2})` }}
+      ></div>
+      */}
+
+          {/* Animated Background  */}
+          {/* <div className="absolute inset-0 animate-waves bg-cover bg-center" style={{ backgroundImage: `url(${blkchain2})` }}></div> */}
+
+          <div
+            className="absolute inset-0 animate-moving-bg bg-cover bg-center"
+            style={{ backgroundImage: `url(${blkchain5})` }}
+          ></div>
+          <div className="stars"></div>
+
+          <div className="bg-zinc-200 p-6 rounded-lg shadow-lg w-2/3 lg:w-2xl relative">
+            <h2 className="text-xl font-bold mb-4">Manage Tokens</h2>
+            <p>Here you can manage your tokens.</p>
+            <div className="flex justify-between w-full">
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-cyan-700 text-white rounded-md"
+          
               >
                 Connect Wallet
               </button>
               <button
                 className=" hover:cursor-pointer mt-4 px-4 py-2 bg-green-700 text-white rounded-md"
-                onClick={() => setManageTokensModalOpen(false)}
-              >
+             >
                 Check your WiseCoin
               </button>
             </div>
             <button
-                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
-                onClick={() => setManageTokensModalOpen(false)}
-              >
-                Cancel
-              </button>
+              className=" hover:cursor-pointer mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
+              onClick={() => setP2pModalOpen(false)}
+            >
+              Cancel
+            </button>
           </div>
         </div>
       )}
+      {/* Scehdule Payment Modal */}
+      {schedulePayModalOpen && (
+        <div className="fixed inset-0 text-black flex items-center justify-center bg-opacity-50 z-50">
+          {/* Moving Gradient Background */}
+          {/* <div className="absolute inset-0 animate-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-cyan-500"></div> */}
+
+          {/* Moving Image Background */}
+          {/* <div
+        className="absolute inset-0 galaxy-background bg-contain "
+         style={{ backgroundImage: `url(${blkchain2})` }}
+      ></div>
+      */}
+
+          {/* Animated Background  */}
+          {/* <div className="absolute inset-0 animate-waves bg-cover bg-center" style={{ backgroundImage: `url(${blkchain2})` }}></div> */}
+
+          <div
+            className="absolute inset-0 animate-moving-bg bg-cover bg-center"
+            style={{ backgroundImage: `url(${blkchain5})` }}
+          ></div>
+          <div className="stars"></div>
+
+          <div className="bg-zinc-200 p-6 rounded-lg shadow-lg w-2/3 lg:w-2xl relative">
+            <h2 className="text-xl font-bold mb-4">Manage Tokens</h2>
+            <p>Here you can manage your tokens.</p>
+            <div className="flex justify-between w-full">
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-cyan-700 text-white rounded-md"
+                >
+                Connect Wallet
+              </button>
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-green-700 text-white rounded-md"
+                >
+                Check your WiseCoin
+              </button>
+            </div>
+            <button
+              className=" hover:cursor-pointer mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
+              onClick={() => setSchedulePayModalOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+      {/* AutoPay Modal */}
+      {autoPayModalOpen && (
+        <div className="fixed inset-0 text-black flex items-center justify-center bg-opacity-50 z-50">
+         
+          <div
+            className="absolute inset-0 animate-moving-bg bg-cover bg-center"
+            style={{ backgroundImage: `url(${blkchain5})` }}
+          ></div>
+          <div className="stars"></div>
+
+          <div className="bg-zinc-200 p-6 rounded-lg shadow-lg w-2/3 lg:w-2xl relative">
+            <h2 className="text-xl font-bold mb-4">Manage Tokens</h2>
+            <p>Here you can manage your tokens.</p>
+            <div className="flex justify-between w-full">
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-cyan-700 text-white rounded-md"
+                >
+                Connect Wallet
+              </button>
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-green-700 text-white rounded-md"
+                >
+                Check your WiseCoin
+              </button>
+            </div>
+            <button
+              className=" hover:cursor-pointer mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
+              onClick={() => setAutoPayModalOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
+
+
+
+
+
+
+
+
+       {/* P2P Modal */}
+       {/* {p2pModalOpen && (
+        <div className="fixed inset-0 text-black flex items-center justify-center z-50 bg-neutral-500 bg-opacity-50">
+            <div
+            className="absolute inset-0 animate-moving-bg bg-cover bg-center"
+            style={{ backgroundImage: `url(${blkchain2})` }}
+          ></div>
+          <div className="stars"></div>
+          <div className="bg-white p-6 rounded-lg shadow-lg w-2/3 md:w-1/3">
+            <h2 className="text-xl font-bold mb-4">Fund Wallet</h2>
+            <p>Enter the amount you want to add to your wallet.</p>
+            <div className="flex justify-between">
+              <button
+                className="mt-4 px-4 py-2  hover:cursor-pointer bg-red-500 text-white rounded"
+                onClick={() => setFundModalOpen(false)}
+              >
+                Cancel
+              </button>
+              <button
+                className=" hover:cursor-pointer mt-4 px-4 py-2 bg-green-800 text-white rounded"
+                onClick={() => setFundModalOpen(false)}
+              >
+                Pay Now
+              </button>
+            </div>
+          </div>
+        </div>
+      )} */}
     </section>
   );
 };
