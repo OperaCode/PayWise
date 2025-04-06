044bbb9bb77f6825655c5cecafcacb3f154ee180
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-04-06 23:31:33 +0100

    charydebug

--- client/src/charts/PieChart.jsx
+++ client/src/charts/PieChart.jsx
@@ -1,55 +1,4 @@
 
-// import React from 'react'
-// import {Chart, ArcElement} from "chart.js"
-// import {Doughnut} from "react-chartjs-2";
-// import Label from './Labels';
-
-// Chart.register(ArcElement);
-
-// const config = {
-//   data: {
-//     datasets: [{
-//       data: [300, 50, 100],
-//       backgroundColor: [
-//         'rgb(255, 99, 132)',
-//         'rgb(54, 162, 235)',
-//         'rgb(255, 205, 86)'
-//       ],
-//       hoverOffset: 4,
-//       borderRadius:7,
-//       spacing:5
-//     }]
-//   },
-//   options:{
-//     responsiveness:true,
-//     cutout:60,  
-//     }
-// }
-// const PieChart = () => {
-//   return (
-   
-
-//     <div className=' m-auto rounded'>
-//         <h2 className='font-bold text-xl p-2 flex  justify-center '>Expense Breakdown</h2>
-//       <div className="items-center flex w-lg justify-center m-auto ">
-//         <div className="chart w-60 h-60 relative m-auto">
-//          <Doughnut {...config}></Doughnut>
-//          <h3 className='mb-4 font-bold title'>Total <span className='block text-3xl text-emerald-400'>${0}</span></h3>
-//         </div>
-        
-//           {/* labels */}
-//           <Label></Label>
-       
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default PieChart
-
-
-
-
 import React from 'react'
 import { Chart, ArcElement, Tooltip, Legend } from "chart.js"
 import { Doughnut } from "react-chartjs-2"
@@ -117,7 +66,6 @@ const PieChart = ({ payments }) => {
             <span className='block text-3xl text-emerald-400'>${totalAmount}</span>
           </h3>
         </div>
-        {/* Optional: You can pass serviceTypes and amounts to Label if needed */}
         <Label labels={serviceTypes} values={amounts} />
       </div>
     </div>
