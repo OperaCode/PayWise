0a2ae012e9db1e2f29cfa48438d2c1bc550a0b7e
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-06-29 14:56:51 +0100

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
   return (
     <div className={`app-container ${theme}`}>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage/>}/>
         <Route
           path="/about"
           element={
